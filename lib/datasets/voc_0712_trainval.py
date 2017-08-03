96323652a248eacbc8461039ea14a82b51c21bc1
Yang <1013608889@qq.com>
2017-08-03 12:40:36 +0800

    Update voc_0712_trainval.py

--- lib/datasets/voc_0712_trainval.py
+++ lib/datasets/voc_0712_trainval.py
@@ -23,14 +23,15 @@ class voc_0712_trainval(imdb):
     def __init__(self):
         imdb.__init__(self, 'voc_0712_trainval')
         self.data_root = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL')
-        self.imageset_root = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL/detection_set')
-        self.source = '2007trainval_2012trainval_image2xml.txt'
-        self.img_set = '2007trainval_2012trainval.txt'
+        self.source = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL/detection_set', '2007trainval_2012trainval_image2xml.txt')
+        self.img_set = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL/detection_set', '2007trainval_2012trainval.txt')
 
         assert os.path.exists(self.data_root), \
                 'Data root path does not exist: {}'.format(self.data_root)
-        assert os.path.exists(self.imageset_root), \
-                'Image set root path does not exist: {}'.format(self.imageset_root)
+        assert os.path.exists(self.source), \
+                'Source file does not exist: {}'.format(self.source)
+        assert os.path.exists(self.img_set), \
+                'Image set file does not exist: {}'.format(self.img_set)
 
         self._classes = ('__background__', # always index 0
                          'aeroplane', 'bicycle', 'bird', 'boat',
@@ -57,7 +58,7 @@ class voc_0712_trainval(imdb):
         """
         Load the indexes listed in this dataset's image set file.
         """
-	f = open(self.imageset_root + self.source, 'r')
+	f = open(self.source, 'r')
         for i in f:
             self.image_path_list.append(self.data_root + i.strip().split(' ')[0])
             self.xml_path_list.append(self.data_root + i.strip().split(' ')[1])
@@ -85,10 +86,7 @@ class voc_0712_trainval(imdb):
         """
         Load the indexes listed in this dataset's image set file.
         """
-        image_set_file = os.path.join(self.imageset_root + self.img_set)
-        assert os.path.exists(image_set_file), \
-                'Path does not exist: {}'.format(image_set_file)
-        with open(image_set_file) as f:
+        with open(self.img_set) as f:
             image_index = [x.strip() for x in f.readlines()]
 
         return image_index
