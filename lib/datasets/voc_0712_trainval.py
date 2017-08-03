a5015b5457d10a328411aa589c1db6925c4ec687
Yang <1013608889@qq.com>
2017-08-03 12:15:44 +0800

    Update voc_0712_trainval.py

--- lib/datasets/voc_0712_trainval.py
+++ lib/datasets/voc_0712_trainval.py
@@ -22,8 +22,8 @@ from fast_rcnn.config import cfg
 class voc_0712_trainval(imdb):
     def __init__(self):
         imdb.__init__(self, 'voc_0712_trainval')
-        self.data_root = '/home/prmct/Database/VOC_PASCAL/'
-        self.imageset_root = '/home/prmct/Database/VOC_PASCAL/detection_set/'
+        self.data_root = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL')
+        self.imageset_root = os.path.join(cfg.DATABASE_ROOT, 'VOC_PASCAL/detection_set')
         self.source = '2007trainval_2012trainval_image2xml.txt'
         self.img_set = '2007trainval_2012trainval.txt'
 
