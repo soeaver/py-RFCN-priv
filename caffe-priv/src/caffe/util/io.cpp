a1683e11ae9322cf8d25ae6b6ddd6f476cdc1dab
Yang <1013608889@qq.com>
2017-08-05 00:46:10 +0800

    Update io.cpp

--- caffe-priv/src/caffe/util/io.cpp
+++ caffe-priv/src/caffe/util/io.cpp
@@ -71,7 +71,8 @@ void WriteProtoToBinaryFile(const Message& proto, const char* filename) {
 
 #ifdef USE_OPENCV
 cv::Mat ReadImageToCVMat(const string& filename,
-    const int height, const int width, const bool is_color) {
+			 const int height, const int width, const bool is_color,
+			 int* img_height, int* img_width) {
   cv::Mat cv_img;
   int cv_read_flag = (is_color ? CV_LOAD_IMAGE_COLOR :
     CV_LOAD_IMAGE_GRAYSCALE);
@@ -85,6 +86,13 @@ cv::Mat ReadImageToCVMat(const string& filename,
   } else {
     cv_img = cv_img_origin;
   }
+  if (img_height != NULL) {
+    *img_height = cv_img.rows;
+  }
+  if (img_width != NULL) {
+    *img_width = cv_img.cols;
+  }
+
   return cv_img;
 }
 
