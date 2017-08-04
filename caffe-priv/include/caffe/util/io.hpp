ee517fd89af911956c7a6f4e7c578020b5f9cac0
Yang <1013608889@qq.com>
2017-08-05 00:44:08 +0800

    Update io.hpp

--- caffe-priv/include/caffe/util/io.hpp
+++ caffe-priv/include/caffe/util/io.hpp
@@ -131,7 +131,8 @@ bool DecodeDatum(Datum* datum, bool is_color);
 
 #ifdef USE_OPENCV
 cv::Mat ReadImageToCVMat(const string& filename,
-    const int height, const int width, const bool is_color);
+			 const int height, const int width, const bool is_color,
+			 int* img_height=NULL, int* img_width=NULL);
 
 cv::Mat ReadImageToCVMat(const string& filename,
     const int height, const int width);
