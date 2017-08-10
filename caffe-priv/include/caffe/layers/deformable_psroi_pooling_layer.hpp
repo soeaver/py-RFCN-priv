8d8aea4a689a67d98d7d842d52a8326ca716f3d6
lzx1413 <lzx1413@live.cn>
2017-08-10 22:56:55 +0800

    typo fix

--- caffe-priv/include/caffe/layers/deformable_psroi_pooling_layer.hpp
+++ caffe-priv/include/caffe/layers/deformable_psroi_pooling_layer.hpp
@@ -9,28 +9,6 @@
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
-
-/** 
- * @brief Perform position-sensitive max pooling on regions of interest specified by input, takes
- *        as input N position-sensitive score maps and a list of R regions of interest.
- *   ROIPoolingLayer takes 2 inputs and produces 1 output. bottom[0] is
- *   [N x (C x K^2) x H x W] position-sensitive score maps on which pooling is performed. bottom[1] is
- *   [R x 5] containing a list R ROI tuples with batch index and coordinates of
- *   regions of interest. Each row in bottom[1] is a ROI tuple in format
- *   [batch_index x1 y1 x2 y2], where batch_index corresponds to the index of
- *   instance in the first input and x1 y1 x2 y2 are 0-indexed coordinates
- *   of ROI rectangle (including its boundaries). The output top[0] is [R x C x K x K] score maps pooled
- *   within the ROI tuples.
- * @param param provides PSROIPoolingParameter psroi_pooling_param,
- *        with PSROIPoolingLayer options:
- *  - output_dim. The pooled output channel number.
- *  - group_size. The number of groups to encode position-sensitive score maps
- *  - spatial_scale. Multiplicative spatial scale factor to translate ROI
- *  coordinates from their input scale to the scale used when pooling.
- * R-FCN
- * Written by Yi Li
- */
-
 template <typename Dtype>
 class DeformablePSROIPoolingLayer : public Layer<Dtype> {
  public:	
