8d93d39cf3e1552fcd0bd8622c48d5e863125578
Yang <1013608889@qq.com>
2017-08-19 08:24:33 +0800

    Update roi_align_layer.cu

--- caffe-priv/src/caffe/layers/roi_align_layer.cu
+++ caffe-priv/src/caffe/layers/roi_align_layer.cu
@@ -1,39 +1,20 @@
-// ------------------------------------------------------------------
-// Project: Mask R-CNN 
-// File: ROIAlignLayer
-// Adopted from roi_pooling_layer.cu (written by Ross Grischik)
-// Author: Jasjeet Dhaliwal
-// ------------------------------------------------------------------
-
 #include <cfloat>
-#include <iostream>
-#include <string>
-#include <utility>
+#include <algorithm>
 #include <vector>
 
-#include <algorithm> 
-#include <stdlib.h> 
-
-#include "caffe/blob.hpp"
-#include "caffe/common.hpp"
-#include "caffe/layer.hpp"
 #include "caffe/layers/roi_align_layer.hpp"
-#include "caffe/proto/caffe.pb.h"
-
 using std::max;
 using std::min;
-using std::floor; 
-using std::ceil; 
-using std::fabs; 
-using std::cout; 
 
 namespace caffe {
+
 
 template <typename Dtype>
 __global__ void ROIAlignForward(const int nthreads, const Dtype* bottom_data,
     const Dtype spatial_scale, const int channels, const int height,
     const int width, const int pooled_height, const int pooled_width,
-    const Dtype* bottom_rois, Dtype* top_data, int* argmax_idx, Dtype* argmax_mult) {
+    const Dtype* bottom_rois, Dtype* top_data, int* argmax_data_x,
+    int* argmax_data_y) {
   CUDA_KERNEL_LOOP(index, nthreads) {
     // (n, c, ph, pw) is an element in the pooled output
     int pw = index % pooled_width;
@@ -41,23 +22,20 @@ __global__ void ROIAlignForward(const int nthreads, const Dtype* bottom_data,
     int c = (index / pooled_width / pooled_height) % channels;
     int n = index / pooled_width / pooled_height / channels;
 
-    int argmax_index = index * 4; 
-
     bottom_rois += n * 5;
     int roi_batch_ind = bottom_rois[0];
     Dtype roi_start_w = bottom_rois[1] * spatial_scale;
     Dtype roi_start_h = bottom_rois[2] * spatial_scale;
     Dtype roi_end_w = bottom_rois[3] * spatial_scale;
     Dtype roi_end_h = bottom_rois[4] * spatial_scale;
-    
-    //Util Values
-    Dtype zero = 0.0, one = 1.0;
- 
-    // Force malformed ROIs to be 1x1
-    Dtype roi_width = max(roi_end_w - roi_start_w + 1.0, one);
-    Dtype roi_height = max(roi_end_h - roi_start_h + 1.0, one);
-    Dtype bin_size_h = roi_height / static_cast<Dtype>(pooled_height);
-    Dtype bin_size_w = roi_width / static_cast<Dtype>(pooled_width);
+
+    Dtype roi_width = roi_end_w - roi_start_w + 1;
+    Dtype roi_height = roi_end_h - roi_start_h + 1;
+
+    Dtype bin_size_h = static_cast<Dtype>(roi_height)
+                       / static_cast<Dtype>(pooled_height);
+    Dtype bin_size_w = static_cast<Dtype>(roi_width)
+                       / static_cast<Dtype>(pooled_width);
 
     Dtype hstart = static_cast<Dtype>(ph) * bin_size_h;
     Dtype wstart = static_cast<Dtype>(pw) * bin_size_w;
@@ -65,96 +43,60 @@ __global__ void ROIAlignForward(const int nthreads, const Dtype* bottom_data,
     Dtype wend = static_cast<Dtype>(pw + 1) * bin_size_w;
 
     // Add roi offsets and clip to input boundaries
-    hstart = min(max(hstart + roi_start_h, zero), static_cast<Dtype>(height) );
-    hend = min(max(hend + roi_start_h, zero), static_cast<Dtype>(height));
-    wstart = min(max(wstart + roi_start_w, zero), static_cast<Dtype>(width));
-    wend = min(max(wend + roi_start_w, zero), static_cast<Dtype>(width));
-    
+    hstart = min(max(hstart + roi_start_h, 0.), static_cast<float>(height));
+    hend = min(max(hend + roi_start_h, 0.), static_cast<float>(height));
+    wstart = min(max(wstart + roi_start_w, 0.), static_cast<float>(width));
+    wend = min(max(wend + roi_start_w, 0.), static_cast<float>(width));
     bool is_empty = (hend <= hstart) || (wend <= wstart);
 
     // Define an empty pooling region to be zero
+    Dtype maxval = is_empty ? 0 : -FLT_MAX;
+    // If nothing is pooled, argmax = -1 causes nothing to be backprop'd
+    int maxidx_x = -1;
+    int maxidx_y = -1;
+    bottom_data += (roi_batch_ind * channels + c) * height * width;
+    for (Dtype h = hstart; h < hend; h += 1.) {
+      for (Dtype w = wstart; w < wend; w += 1.) {
+        // Selecting four regular locations for bilinear interpolation
+        int x_left = floor(w);
+        int x_right = ceil(w);
+        int y_bottom = floor(h);
+        int y_top = ceil(h);
 
-   
+        int top_left_index = y_top * width + x_left;
+        int top_right_index = y_top * width + x_right;
+        int bottom_left_index = y_bottom * width + x_left;
+        int bottom_right_index = y_bottom * width + x_right;
 
-    Dtype maxvalue = is_empty ? 0 : -FLT_MAX;
-    int maxidx[4];
-    Dtype maxmult[4];  
-    //int bottom_offset =  (roi_batch_ind * channels + c) * height * width ;
-    //bottom_data += (roi_batch_ind * channels + c) * height * width;
-    /* Normalization function - normalizes values between -1 and 1. 
-    a = -1, b = 1
-    y = f(x) = [[(b - a) (x - roi_start_h)] / [roi_end_h - roi_start_h]] + a
-    x = f^{-1}(y) = [[(f(x) - a)(roi_end_h - roi_end_h)] / (b - a)] + roi_start_h 
-    Normalized coordinates of 4 regularly sampled points in the ROI: 
-    sn_1 = (-0.5,-0.5) 
-    sn_2 = (-0.5,0.5) 
-    sn_3 = (0.5,-0.5) 
-    sn_4 = (0.5,0.5)
+        bool is_top_left_in = x_left >= 0 && x_left <= width - 1
+            && y_top >= 0 && y_top <= height - 1;
+        bool is_top_right_in = x_right >= 0 && x_right <= width - 1
+            && y_top >= 0 && y_top <= height - 1;
+        bool is_bottom_left_in = x_left >= 0 && x_left <= width - 1
+            && y_bottom >= 0 && y_bottom <= height - 1;
+        bool is_bottom_right_in = x_right >= 0 && x_right <= width - 1
+            && y_bottom >= 0 && y_bottom <= height - 1;
 
-    // Debugging purposes
-    Dtype x_pos = (((0.5 + 1)*(roi_end_w - roi_start_w))/2.0) + roi_start_w;
-    Dtype x_neg = (((-0.5 + 1)*(roi_end_w - roi_start_w))/2.0) + roi_start_w;
-    Dtype y_pos = (((0.5 + 1)*(roi_end_h - roi_start_h))/2.0) + roi_start_h;
-    Dtype y_neg = (((-0.5 + 1)*(roi_end_h - roi_start_h))/2.0) + roi_start_h;
-    Dtype samples[2] = {x_neg, y_neg, x_neg, y_pos,
-                        x_pos, y_neg, x_pos, y_pos};
-    */
-    
-    Dtype samples_n[8] = {-0.5, -0.5, -0.5, 0.5, 
-                           0.5, -0.5, 0.5, 0.5}; 
-    //Holds interpolated values for each sample point
-    Dtype bisampled[4]; 
-    int counter = 0; 
-    Dtype x_smp_n = -2.0, y_smp_n = -2.0, h_idx_n = -2.0, w_idx_n = -2.0;   
-    //Bilinearly Interpolate 4 sampled values
-    for (int smp = 0; smp < sizeof(samples_n)/sizeof(*samples_n) ; smp+=2) {
-      x_smp_n = samples_n[smp]; 
-      y_smp_n = samples_n[smp+1]; 
+        Dtype val = 0;
+        if (is_top_left_in)
+          val += (1 - w + x_left) * (1 - y_top + h) * bottom_data[top_left_index];
+        if (is_top_right_in)
+          val += (1 - x_right + w) * (1 - y_top + h) * bottom_data[top_right_index];
+        if (is_bottom_left_in)
+          val += (1 - w + x_left) * (1 - h + y_bottom) * bottom_data[bottom_left_index];
+        if (is_bottom_right_in)
+          val += (1 - x_right + w) * (1 - h + y_bottom) * bottom_data[bottom_right_index];
 
-      bisampled[smp/2] = 0.0;
-      int b_index[4] = {-1, -1 , -1, -1}; // -1,-1,-1,-1}; 
-      //int b_index_curr[4] = {-1,-1,-1,-1}; 
-      Dtype multiplier[4] = {Dtype(-FLT_MAX), Dtype(-FLT_MAX), Dtype(-FLT_MAX), Dtype(-FLT_MAX)}; 
-					//Dtype(-FLT_MAX), Dtype(-FLT_MAX), Dtype(-FLT_MAX), Dtype(-FLT_MAX)};  
-      counter = 0; 
-      //ceil(hstart)
-      //floor(hend)
-      for (int h_idx = ceil(hstart); h_idx <= floor(hend) && h_idx <= height && h_idx >= 0 ; ++h_idx) {
-        for (int w_idx =ceil(wstart); w_idx <= floor(wend) && w_idx <= width && w_idx >= 0; ++w_idx) {
-       if (counter < 4) {
-            b_index[counter] =  ((((roi_batch_ind * channels) + c) * height) + h_idx) * width + w_idx; 
-        //    b_index_curr[counter]= h_idx*width + w_idx; 
-            //Normalize width and height to lie between -1 and 1
-            h_idx_n = static_cast<Dtype>( (static_cast<Dtype>(2)*(static_cast<Dtype>(h_idx) - roi_start_h) / (roi_end_h - roi_start_h)) - 1); 
-            w_idx_n =  static_cast<Dtype>((static_cast<Dtype>(2)*(static_cast<Dtype>(w_idx) - roi_start_w) / (roi_end_w - roi_start_w))  - 1);
-            h_idx_n = min(max(h_idx_n, static_cast<Dtype>(-1.0)),one);  
-            w_idx_n = min(max(w_idx_n, static_cast<Dtype>(-1.0)),one);  
-            multiplier[counter]=  max(zero ,static_cast<Dtype>(1 - fabs(x_smp_n - w_idx_n))) * max(zero,static_cast<Dtype>(1 - fabs(y_smp_n - h_idx_n)));
-            //bisampled[smp/2] += multiplier[counter]; 
-            bisampled[smp/2] += bottom_data[ b_index[counter]] * multiplier[counter];
-            ++counter; 
-	  } else {
-	     goto stop; 
-	  }
-        } //w
-      }//h
-      stop:
-      if (bisampled[smp/2] > maxvalue) {
-        maxvalue = bisampled[smp/2]; 
-        //Using two loops to comply with c++ convention
-        for (int i=0; i<4;++i) {
-          maxidx[i] = b_index[i];
-          maxmult[i] = multiplier[i]; 
-	}
-
+        if (val > maxval) {
+          maxval = val;
+          maxidx_x = static_cast<int>(w);
+          maxidx_y = static_cast<int>(h);
+        }
       }
-    } //smp
-    //Store value in the top blob
-    top_data[index] = maxvalue;
-    for (int i = 0; i<4; ++i, ++argmax_index) {
-      argmax_idx[argmax_index] = maxidx[i];
-      argmax_mult[argmax_index] = maxmult[i]; 
     }
+    top_data[index] = maxval;
+    argmax_data_x[index] = maxidx_x;
+    argmax_data_y[index] = maxidx_y;
   }
 }
 
@@ -164,22 +106,25 @@ void ROIAlignLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->gpu_data();
   const Dtype* bottom_rois = bottom[1]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
-  int* argmax_idx = max_pts_.mutable_gpu_data();
-  Dtype* argmax_mult = max_mult_.mutable_gpu_data(); 
+  int* argmax_data_x = max_idx_.mutable_gpu_data();
+  int* argmax_data_y = max_idy_.mutable_gpu_data();
+
+  if (bottom.size() > 2) {
+      const Dtype* scale_pred = bottom[2]->gpu_data();
+      caffe_gpu_asum<Dtype>(1, scale_pred, &spatial_scale_);
+  }
   int count = top[0]->count();
-  // LOG(INFO) << "Doing forward now";   
   // NOLINT_NEXT_LINE(whitespace/operators)
-  //Change CAFFE_CUDA_NUM_THREADS to 64
-  ROIAlignForward<Dtype><<<CAFFE_GET_BLOCKS(count), 32>>>(
+  ROIAlignForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, bottom_data, spatial_scale_, channels_, height_, width_,
-      pooled_height_, pooled_width_, bottom_rois, top_data, argmax_idx, argmax_mult);
-   // LOG(INFO) << "Done forward "; 
+      pooled_height_, pooled_width_, bottom_rois, top_data, argmax_data_x, argmax_data_y);
   CUDA_POST_KERNEL_CHECK;
 }
 
 template <typename Dtype>
-__global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
-    const int* argmax_idx, const Dtype* argmax_mult, const int num_rois, const Dtype spatial_scale,
+__global__ void ROIAlignBackward(
+    const int nthreads, const Dtype* top_diff, const int* argmax_data_x,
+    const int* argmax_data_y, const int num_rois, const Dtype spatial_scale,
     const int channels, const int height, const int width,
     const int pooled_height, const int pooled_width, Dtype* bottom_diff,
     const Dtype* bottom_rois) {
@@ -189,16 +134,10 @@ __global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
     int h = (index / width) % height;
     int c = (index / width / height) % channels;
     int n = index / width / height / channels;
-    
-    Dtype gradient = 0.0;
+
+    Dtype gradient = 0;
     // Accumulate gradient over all ROIs that pooled this element
     for (int roi_n = 0; roi_n < num_rois; ++roi_n) {
-      //const Dtype* offset_bottom_rois = bottom_rois + roi_n * 5;
-      //int roi_batch_ind = offset_bottom_rois[0];
-      // Skip if ROI's batch index doesn't match n
-     // if (n != roi_batch_ind) {
-       // continue;
-     // }
       const Dtype* offset_bottom_rois = bottom_rois + roi_n * 5;
       int roi_batch_ind = offset_bottom_rois[0];
       // Skip if ROI's batch index doesn't match n
@@ -206,10 +145,11 @@ __global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
         continue;
       }
 
-      int roi_start_w = ceil(offset_bottom_rois[1] * spatial_scale);
-      int roi_start_h = ceil(offset_bottom_rois[2] * spatial_scale);
-      int roi_end_w = floor(offset_bottom_rois[3] * spatial_scale);
-      int roi_end_h = floor(offset_bottom_rois[4] * spatial_scale);
+      // And it assumes that we don't have any negative offset of course
+      int roi_start_w = floor(offset_bottom_rois[1] * spatial_scale);
+      int roi_start_h = floor(offset_bottom_rois[2] * spatial_scale);
+      int roi_end_w = ceil(offset_bottom_rois[3] * spatial_scale);
+      int roi_end_h = ceil(offset_bottom_rois[4] * spatial_scale);
 
       // Skip if ROI doesn't include (h, w)
       const bool in_roi = (w >= roi_start_w && w <= roi_end_w &&
@@ -219,23 +159,56 @@ __global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
       }
 
       int offset = (roi_n * channels + c) * pooled_height * pooled_width;
-      int argmax_offset = offset * 4; 
       const Dtype* offset_top_diff = top_diff + offset;
-      const int* offset_argmax_idx = argmax_idx + argmax_offset;
-      const Dtype* offset_argmax_mult = argmax_mult + argmax_offset;
-      // Util Vals
-      Dtype multiplier = 0.0; 
-      for (int ph = 0; ph < pooled_height; ++ph) {
-        for (int pw = 0; pw < pooled_width; ++pw) {
-          for (int k = 0; k < 4; ++k) {
-            if (offset_argmax_idx[((ph * pooled_width + pw) * 4) + k] == index  ) {
-              multiplier = offset_argmax_mult[( (ph * pooled_width + pw) * 4) + k]; 
-              gradient += offset_top_diff[ph * pooled_width + pw] * multiplier;
-            }
-	  }
-        }//pw
-      }//ph
-    }//rois
+      const int* offset_argmax_data_x = argmax_data_x + offset;
+      const int* offset_argmax_data_y = argmax_data_y + offset;
+
+      // Compute feasible set of pooled units that could have pooled
+      // this bottom unit
+      Dtype roi_width = roi_end_w - roi_start_w + 1;
+      Dtype roi_height = roi_end_h - roi_start_h + 1;
+
+      Dtype bin_size_h = static_cast<Dtype>(roi_height)
+                         / static_cast<Dtype>(pooled_height);
+      Dtype bin_size_w = static_cast<Dtype>(roi_width)
+                         / static_cast<Dtype>(pooled_width);
+
+      int phstart = floor(static_cast<Dtype>(h - roi_start_h) / bin_size_h);
+      int phend = ceil(static_cast<Dtype>(h - roi_start_h + 1) / bin_size_h);
+      int pwstart = floor(static_cast<Dtype>(w - roi_start_w) / bin_size_w);
+      int pwend = ceil(static_cast<Dtype>(w - roi_start_w + 1) / bin_size_w);
+
+      phstart = min(max(phstart, 0), pooled_height);
+      phend = min(max(phend, 0), pooled_height);
+      pwstart = min(max(pwstart, 0), pooled_width);
+      pwend = min(max(pwend, 0), pooled_width);
+
+      for (int ph = phstart; ph <= phend; ++ph) {
+        for (int pw = pwstart; pw <= pwend; ++pw) {
+          int index = ph * pooled_width + pw;
+          Dtype max_x = offset_argmax_data_x[index];
+          Dtype max_y = offset_argmax_data_y[index];
+
+          int x_left = floor(max_x);
+          int x_right = ceil(max_x);
+          int y_bottom = floor(max_y);
+          int y_top = ceil(max_y);
+
+          if (x_left == w && y_top == h)
+            gradient += (1 - max_x + x_left) * (1 - y_top + max_y)
+                * offset_top_diff[index];
+          else if (x_left == w && y_bottom == h)
+            gradient += (1 - max_x + x_left) * (1 - max_y + y_bottom)
+                * offset_top_diff[index];
+          else if (x_right == w && y_top == h)
+            gradient += (1 - x_right + max_x) * (1 - y_top + max_y)
+                * offset_top_diff[index];
+          else if (x_right == w && y_bottom == h)
+            gradient += (1 - x_right + max_x) * (1 - max_y + y_bottom)
+                * offset_top_diff[index];
+        }
+      }
+    }
     bottom_diff[index] = gradient;
   }
 }
@@ -251,18 +224,20 @@ void ROIAlignLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
   const int count = bottom[0]->count();
   caffe_gpu_set(count, Dtype(0.), bottom_diff);
-  const int* argmax_idx = max_pts_.gpu_data();
-  const Dtype* argmax_mult = max_mult_.gpu_data(); 
+  const int* argmax_data_x = max_idx_.gpu_data();
+  const int* argmax_data_y = max_idy_.gpu_data();
+
+  if (bottom.size() > 2) {
+      const Dtype* scale_pred = bottom[2]->gpu_data();
+      caffe_gpu_asum<Dtype>(1, scale_pred, &spatial_scale_);
+  }
   // NOLINT_NEXT_LINE(whitespace/operators)
-  // CAFFE_CUDA_NUM_THREADS replaced with 64
-  // LOG(INFO) << "Doing backward "; 
-  ROIAlignBackward<Dtype><<<CAFFE_GET_BLOCKS(count), 16>>>(
-      count, top_diff, argmax_idx, argmax_mult, top[0]->num(), spatial_scale_, channels_,
+  ROIAlignBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+      count, top_diff, argmax_data_x, argmax_data_y, top[0]->num(), spatial_scale_, channels_,
       height_, width_, pooled_height_, pooled_width_, bottom_diff, bottom_rois);
-  // LOG(INFO) << "Done backward"; 
   CUDA_POST_KERNEL_CHECK;
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(ROIAlignLayer);
 
-}  // namespace caffe
+}
