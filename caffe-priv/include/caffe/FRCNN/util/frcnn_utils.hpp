74fe77688b8550b55ddbeb6843b74a868b7ae5b1
Yang <1013608889@qq.com>
2017-11-07 04:51:45 -0600

    Update frcnn_utils.hpp

--- caffe-priv/include/caffe/FRCNN/util/frcnn_utils.hpp
+++ caffe-priv/include/caffe/FRCNN/util/frcnn_utils.hpp
@@ -22,7 +22,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/contrib/contrib.hpp>
+// #include <opencv2/contrib/contrib.hpp>
 #include <boost/algorithm/string.hpp>
 #include "boost/filesystem.hpp"
 
