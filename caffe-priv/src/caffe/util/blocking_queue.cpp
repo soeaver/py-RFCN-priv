e9cf8db0185f3da3fc4ab145ee61e06f49c25c6e
Yang <1013608889@qq.com>
2017-11-08 11:54:36 +0800

    Add files via upload

--- caffe-priv/src/caffe/util/blocking_queue.cpp
+++ caffe-priv/src/caffe/util/blocking_queue.cpp
@@ -1,6 +1,7 @@
 #include <boost/thread.hpp>
 #include <string>
 
+#include "caffe/data_reader.hpp"
 #include "caffe/layers/base_data_layer.hpp"
 #include "caffe/parallel.hpp"
 #include "caffe/util/blocking_queue.hpp"
@@ -87,5 +88,10 @@ size_t BlockingQueue<T>::size() const {
 
 template class BlockingQueue<Batch<float>*>;
 template class BlockingQueue<Batch<double>*>;
+template class BlockingQueue<Datum*>;
+template class BlockingQueue<AnnotatedDatum*>;
+template class BlockingQueue<shared_ptr<DataReader<Datum>::QueuePair> >;
+template class BlockingQueue<
+  shared_ptr<DataReader<AnnotatedDatum>::QueuePair> >;
 
 }  // namespace caffe
