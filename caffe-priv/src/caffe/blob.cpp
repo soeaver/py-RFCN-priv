ecf5806903128aa9bee624747a7352810a356299
Yang <1013608889@qq.com>
2017-11-09 11:51:24 +0800

    Add files via upload

--- caffe-priv/src/caffe/blob.cpp
+++ caffe-priv/src/caffe/blob.cpp
@@ -169,7 +169,6 @@ void Blob<Dtype>::ShareDiff(const Blob& other) {
 // Blob<int> or Blob<unsigned int>.
 template <> void Blob<unsigned int>::Update() { NOT_IMPLEMENTED; }
 template <> void Blob<int>::Update() { NOT_IMPLEMENTED; }
-template <> void Blob<bool>::Update() { NOT_IMPLEMENTED; }
 
 template <typename Dtype>
 void Blob<Dtype>::Update() {
@@ -203,11 +202,6 @@ template <> unsigned int Blob<unsigned int>::asum_data() const {
 }
 
 template <> int Blob<int>::asum_data() const {
-  NOT_IMPLEMENTED;
-  return 0;
-}
-
-template <> bool Blob<bool>::asum_data() const {
   NOT_IMPLEMENTED;
   return 0;
 }
@@ -247,11 +241,6 @@ template <> int Blob<int>::asum_diff() const {
   return 0;
 }
 
-template <> bool Blob<bool>::asum_diff() const {
-  NOT_IMPLEMENTED;
-  return 0;
-}
-
 template <typename Dtype>
 Dtype Blob<Dtype>::asum_diff() const {
   if (!diff_) { return 0; }
@@ -283,11 +272,6 @@ template <> unsigned int Blob<unsigned int>::sumsq_data() const {
 }
 
 template <> int Blob<int>::sumsq_data() const {
-  NOT_IMPLEMENTED;
-  return 0;
-}
-
-template <> bool Blob<bool>::sumsq_data() const {
   NOT_IMPLEMENTED;
   return 0;
 }
@@ -329,11 +313,6 @@ template <> int Blob<int>::sumsq_diff() const {
   return 0;
 }
 
-template <> bool Blob<bool>::sumsq_diff() const {
-  NOT_IMPLEMENTED;
-  return 0;
-}
-
 template <typename Dtype>
 Dtype Blob<Dtype>::sumsq_diff() const {
   Dtype sumsq;
@@ -369,10 +348,6 @@ template <> void Blob<int>::scale_data(int scale_factor) {
   NOT_IMPLEMENTED;
 }
 
-template <> void Blob<bool>::scale_data(bool scale_factor) {
-  NOT_IMPLEMENTED;
-}
-
 template <typename Dtype>
 void Blob<Dtype>::scale_data(Dtype scale_factor) {
   Dtype* data;
@@ -403,10 +378,6 @@ template <> void Blob<unsigned int>::scale_diff(unsigned int scale_factor) {
 }
 
 template <> void Blob<int>::scale_diff(int scale_factor) {
-  NOT_IMPLEMENTED;
-}
-
-template <> void Blob<bool>::scale_diff(bool scale_factor) {
   NOT_IMPLEMENTED;
 }
 
@@ -583,7 +554,6 @@ void Blob<float>::ToProto(BlobProto* proto, bool write_diff) const {
 }
 
 INSTANTIATE_CLASS(Blob);
-template class Blob<bool>;
 template class Blob<int>;
 template class Blob<unsigned int>;
 
